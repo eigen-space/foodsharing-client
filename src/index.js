f44e36faa18db398278b3a13548a870138fdd847
Nikita Agupov <nktrsk@gmail.com>
2020-06-20 13:07:20 +0300

    common: fixed geolocation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { environment } = require('./environments/environment');
 const { BotEntryPoint } = require('./services/app/bot-entry-point/bot-entry-point');
 
-const botStarter = new BotEntryPoint({ token: environment.token });
+const botStarter = new BotEntryPoint({ token: environment.vk.token });
 
 botStarter.launch();
